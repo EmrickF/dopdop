33ca302a166256fbbadf7279cf62f704afbf2e8d
EmrickF <erik.finnberg07@gmail.com>
2025-08-13 13:55:58 +0300

    drilla

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface CardProps {
+  title: string;
+  description: string;
+};
+
+export default function Card({ title, description }: CardProps) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </div>
+  );
+}
